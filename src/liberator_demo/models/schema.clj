1c63ec34303b9caaa06dd92334e7471ca203374e
Norman Richards <orb@nostacktrace.com>
2014-02-03 10:50:01 -0600

    schema

--- src/liberator_demo/models/schema.clj
+++ src/liberator_demo/models/schema.clj
@@ -19,18 +19,41 @@
   []
   (.exists (new java.io.File (str (io/resource-path) db-store ".h2.db"))))
 
-(defn create-users-table
-  []
+(defn create-users-table []
   (sql/with-connection db-spec
     (sql/create-table :users
                       [:id "varchar(20) PRIMARY KEY"]
-                      [:first_name "varchar(30)"]
-                      [:last_name "varchar(30)"]
-                      [:email "varchar(30)"] [:admin :boolean]
-                      [:last_login :time] [:is_active :boolean]
-                      [:pass "varchar(100)"])))
+                      [:email "varchar(30)"]
+                      [:admin :boolean]
+                      [:pass "varchar(100)"]
+                      [:api_key "varchar(16)"])))
 
-(defn create-tables
-  "creates the database tables used by the application"
-  []
-  (create-users-table))
+(defn create-games-table []
+  (sql/with-connection db-spec
+    (sql/create-table :games
+                      [:id "identity not null primary key"]
+                      [:name "varchar(128) not null"]
+                      [:description "varchar(1024)"]
+                      [:url "varchar(256)"]
+                      [:owner "varchar(20) not null"]
+                      ["foreign key (owner) references users(id)"])))
+
+(defn create-scores-table []
+  (sql/with-connection db-spec
+    (sql/create-table :scores
+                      [:score "bigint not null"]
+                      [:initials "varchar(3) not null"]
+                      [:game "bigint not null"]
+                      ["foreign key (game) references games(id)"])))
+
+(defn drop-tables []
+  (sql/with-connection db-spec
+    (sql/do-commands
+     "drop table if exists scores"
+     "drop table if exists games"
+     "drop table if exists users")))
+
+(defn create-tables []
+  (create-users-table)
+  (create-games-table)
+  (create-scores-table))
